5b52986fd923490c16cadf6b1932799b044a7bb6
ZoneTwelve <zonetwelve159@gmail.com>
2019-10-10 21:46:59 +0800

    [add] DS01 testcase

--- 1.Addition-of-polynomial/test/testcase/generate.js
+++ 1.Addition-of-polynomial/test/testcase/generate.js
@@ -2,31 +2,34 @@ const fs = require("fs");
 const { exec } = require('child_process');
 
 function program(i){
-  exec(`./a.out < ${i}.in > ${i}>.out`, (error, stdout, stderr) => {
+  exec(`./program.out < ${i}.in > ${i}.out`, (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
+      //console.error(`exec error: ${error}`);
       return;
     }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    //console.log(`stdout: ${stdout}`);
+    //console.log(`stderr: ${stderr}`);
   });
 }
 
 function generate(){
-  
-}
-var str;
-for(var n=0;n<100;n++){
-  var len = ~~(Math.random()*-20)+~~(Math.random()*30);
-  str = [`${~~(Math.random()*10)}x^${~~(Math.random()*10)+50}`];
-  console.log(len);
-  for(var i=~~(Math.random()*30)+10;i>len;i--){
-    var k = ~~(Math.random()*6000-1200)+(Math.random()>.5?(~~(Math.random()*500))/1000:0);
-    var p = ~~(Math.random()*(n-i));
-    k = k>0?`+${k}`:k;
-    
-    str.push(`${k}${p==0?"":"x^"+p}`);
+  var max = ~~(Math.random()*1000)+24;
+  var k = ~~(Math.random()*2048)-1024;
+  if(k==0)k = 2;
+  var arr = [`${Math.abs(k)==1?"":k}x^${max}`];
+  for(max-=~~(Math.random()*10+10);max>=0;max-=(~~(Math.random()*10)+1)){
+    var c = ~~(Math.random()*2048)-1024, e = max;
+    if(c==0)c = 3;
+    arr.push(`${c>0?"+":""}${c}${max==0?"":"x^"}${max!=0?max:""}`);
   }
-  console.log(str.join(""));
-  
+  return arr.join("");
 }
+//return console.log(generate().join("\n"));
+for(var i=0;i<18;i++){
+  var testcase = `${generate()}
+${generate()}`;
+  console.log(i, testcase);
+  fs.writeFileSync(`${i+1}.in`, testcase);
+  program(i+1);
+}
+//console.log(generate());
